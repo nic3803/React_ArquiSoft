8eb71a3fe4b2247ec57ea30c7f189fb1d7318141
nic3803 <nicoprandi68@gmail.com>
2022-06-05 18:34:40 -0300

    Se agrego funciones a contador

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -2,14 +2,30 @@ import { type } from "@testing-library/user-event/dist/type";
 import { useReducer, useState } from "react"
 
 const initialState = {contador: 0};
+
+const TYPES = {
+
+    INCREMENT: "INCREMENT",
+    DECREMENT: "DECREMENT",
+    RESET: "RESET"
+
+
+}
+
 function reducer(state,action){
     switch (action.type) {
-        case "INCREMENT":
+
+        
+
+        case TYPES.INCREMENT:
             return {contador: state.contador + 1}
             
     
-        case "DECREMENT":
+        case TYPES.DECREMENT:
             return {contador: state.contador - 1}
+
+        case TYPES.RESET:
+            return initialState;
             
         default:
             return state;
@@ -29,7 +45,7 @@ const Contador = () =>{
    // const sumar = () => setContador(contador  + 1)
     const sumar = () => dispacht({type:"INCREMENT"});
     const restar = () => dispacht({type:"DECREMENT"});
-
+    const resetear = () => dispacht({type:"RESET"})
     return(
         <div style={{textAlight:"center"}}>
            <h2> Contador Reducer</h2>    
@@ -37,8 +53,9 @@ const Contador = () =>{
             <nav>
 
                 <button onClick={sumar}>  +  </button>
+                <button onClick={resetear}>    0     </button>
                 <button onClick={restar}>  -  </button>
-
+                
 
 
             </nav>
